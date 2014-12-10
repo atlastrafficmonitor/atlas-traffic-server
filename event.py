a7562e18507d85e6caf2ee69bda139d2e154ec9d
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-12-10 20:47:59 +0000

    Adapt and prepare Event for Arduino readings abstraction

--- event.py
+++ event.py
@@ -1,9 +1,8 @@
-from pymongo import MongoClient
+import random
+import statistics
 import time
 
-client = MongoClient('atlas-installation.ianks.com', 27017)
-db = client.atlas_traffic_monitor
-collection = db.event
+from pymongo import MongoClient
 
 # Event
 # ----
@@ -11,16 +10,22 @@ collection = db.event
 # An event is the abstract interface for an event containing the information
 # necessary for the visual system. It has methods to deal with serializing the
 # data into a consumable format by the client, and logic for saving the events
-# to a persistent data store (MongoDB).
+# to a persistent data store (MongoDB). It is important to note than an Event,
+# as described by the interface, consists of two data readings from an Arduino.
+
+
+client = MongoClient('atlas-installation.ianks.com', 27017)
+db = client.atlas_traffic_monitor
+collection = db.event
 
 
 class Event:
-    def __init__(self, event_type, pressure_reading):
-        self.event_type = event_type
+    def __init__(self, readings):
+        self.event_type = self._determine_event_type_from_readings(readings)
+        self.pressure_reading_avg = self._pressure_reading_avg(readings)
         self.gmtime = time.time()
-        self.pressure_reading = pressure_reading
         self.strftime = time.strftime("%I:%M")
-
+        self.readings = [r.serialize() for r in readings]
 
     def serialize(self):
         return {
@@ -32,3 +37,18 @@ class Event:
 
     def mongoify(self):
         return collection.insert(self.serialize())
+
+    def _determine_event_type_from_readings(self, readings):
+        first = self.readings[0].sensor_location
+        second = self.readings[1].sensor_location
+
+        # If the first reading is from a front sensor, that means entry.
+        if first  == 'front' and second =='back':
+            return 'entry'
+        elif first  == 'back' and second == 'front':
+            return 'exit'
+        else:
+            return random.sample(['entry', 'exit'], 1)
+
+    def _pressure_reading_avg(self, readings):
+        return mean([r.pressure_amplitude for r in readings])
