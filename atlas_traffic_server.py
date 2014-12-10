155cec037f672db673331eb6f0cbb759b9c0f48a
thefyk <michaelfyk@gmail.com>
2014-12-09 20:46:42 -0700

    Update atlas_traffic_server.py

--- atlas_traffic_server.py
+++ atlas_traffic_server.py
@@ -3,16 +3,35 @@ import signal
 import sys
 import time
 import websockets
+import json
 from IPython import embed
+from random import randint
+
+i = 0
 
 @asyncio.coroutine
+
+
+
 def handler(websocket, path):
     while True:
         if not websocket.open:
             break
 
-        yield from websocket.send("Hello!")
-        yield from asyncio.sleep(1.0)
+        global i
+        i = i + .5
+
+        if i == 10:
+            i= 0
+
+        yield from websocket.send(json.dumps({
+  "totalEntries" : str(1),
+  "secondsSinceLastEntry" : "1.25",
+  #"timestamp" : time.strftime("%H:%M"),
+  "timestamp" : "0" + str(i) + ":00",
+  "swarm" : "true"
+}))
+        yield from asyncio.sleep(1)
 
 def boot_server(host, port):
     print('Atlas server is listening on http://', host, ':', port, sep='')
@@ -20,7 +39,7 @@ def boot_server(host, port):
     return websockets.serve(handler, host, port)
 
 def main():
-    server = boot_server('0.0.0.0', 8765)
+    server = boot_server('0.0.0.0', 3000)
     event_loop = asyncio.get_event_loop()
 
     try:
